d4202018601b9eed1fda5ae0da53435c5b929c8e
xorusz <imkavin96@gmail.com>
2026-02-01 01:39:19 +0800

    Push Script Updated

--- push.sh
+++ push.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# 1. Ask for a commit message
 echo "Enter commit message:"
 read message
 
-# 2. Add all changes
 git add .
 
-# 3. Commit with the provided message
 git commit -m "$message"
 
-# 4. Push to the main branch
 git push origin main
 
 echo "---------------------------------------"
